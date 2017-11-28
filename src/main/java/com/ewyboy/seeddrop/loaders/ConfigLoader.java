98719e192999d6accc6e3d3f80dc9c5de26777f1
Jason Taylor <jason@codetaylor.com>
2017-11-28 09:36:16 -0800

    added meta to config description

--- src/main/java/com/ewyboy/seeddrop/loaders/ConfigLoader.java
+++ src/main/java/com/ewyboy/seeddrop/loaders/ConfigLoader.java
@@ -28,7 +28,7 @@ public class ConfigLoader {
         Configuration config = new Configuration(configurationFile);
         customDrops = config.getStringList("SeedDrop Drop List", Configuration.CATEGORY_GENERAL, customDrops,
                 "How to configure this mod:" + "\n" +
-                "\n" + "First Line: minecraft:dirt  [What to drop when grass is broken]" +
+                "\n" + "First Line: minecraft:dirt:<meta>  [What to drop when grass is broken, meta is optional]" +
                 "\n" + "Second Line: 20  [Drop weight for item/block above (Vanilla Wheat Seeds is 10)]" +
                 "\n" + "You can edit / remove all the entries on this list as well as add new ones." + "\n" + "\n"
         );
